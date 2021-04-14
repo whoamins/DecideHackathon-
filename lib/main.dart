99f07311b7313f72f225f54f26f41f139d18b2d8
nks1ck <nks1ck@mail.ru>
2021-04-14 09:31:30 +0300

    delete all n4xy1

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:decide_hackathon/helpers/dash.dart';
 
 
 void main() => runApp(MyApp());
 
 
 class MyApp extends StatelessWidget {
+  static final String title = 'Холодильник';
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'StorageApp',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: Dash(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: title,
+    theme: ThemeData(
+      primarySwatch: Colors.purple,
+      scaffoldBackgroundColor: Color(0xFFf6f5ee),
+    ),
+    home: HomePage();
+  );
 }
